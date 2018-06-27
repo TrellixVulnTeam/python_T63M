56ba82d26f81cd16109c4ff5bbcb9d0f4df3ce48
imrcrab <imrcrab@163.com>
2018-06-27 23:41:46 +0800

    add python singleton

--- learn/LearnTuple.py
+++ learn/LearnTuple.py
@@ -1,4 +1,7 @@
 # -*- utf-8 -*-
+import threading
+
+
 def one_t():
     tuple_one = ("wang", 1, 2, 3)
     tuple_two = ("li", 22, "liang")
@@ -7,5 +10,33 @@ def one_t():
     print(tuple_three)
 
 
+class Student:
+    '''重要的:------------->>>类和实例'''
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def hello(self):
+        print(self.name, self.age)
+
+
+class Singleton(object):
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        pass
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(Singleton,"_instance"):
+            with Singleton._instance_lock:
+                if not hasattr(Singleton, "_instance"):
+                    Singleton._instance = object.__new__(cls)
+        return Singleton._instance
+
+    def hello(self):
+        print("hello")
+
 if __name__ == "__main__":
-    one_t()
+    Student("wang", 123).hello()
+    Singleton().hello()
